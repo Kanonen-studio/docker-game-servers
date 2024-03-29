d038d5ee412e185349d66427eab2018b593682df
Kanonenbombe <121344478+Kanonenbombe@users.noreply.github.com>
2024-03-29 21:13:46 +0100

    Create Fivem config-db.php

---
+++ fivem/config-db.php
@@ -0,0 +1,25 @@
+<?php
+
+$i++;
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['port'] = '';
+
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = '7wG'3DxWSD9vhxB';
+$cfg['Servers'][$i]['verbose'] = '';
+$cfg['Servers'][$i]['tracking'] = '';
+$cfg['Servers'][$i]['bookmarktable'] = '';
+$cfg['Servers'][$i]['history'] = '';
+$cfg['Servers'][$i]['designer_coords'] = '';
+$cfg['Servers'][$i]['userconfig'] = '';
+$cfg['Servers'][$i]['users'] = '';
+$cfg['Servers'][$i]['recent'] = '';
+$cfg['Servers'][$i]['hotlink'] = '';
+$cfg['Servers'][$i]['table_uiprefs'] = '';
+$cfg['Servers'][$i]['port'] = '3306';
